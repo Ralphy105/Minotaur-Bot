fc23cd600798f76fe27f85d61e0db9ff3f0068d5
Ralphy105 <146513258+Ralphy105@users.noreply.github.com>
2024-08-31 18:10:38 -0500

    Ignore txt files
    - Added choice to consider whitelist or not with privatePrune

--- prunePrivate.js
+++ prunePrivate.js
@@ -2,22 +2,26 @@ const fs = require('node:fs');
 const search = require('./search');
 const parseWhitelist = require('./parseWhitelist');
 
-(async (filename) => {
+(async (filename, filterWhitelist) => {
     let targets = await search(fs.readFileSync(filename,'utf8').toLowerCase().split('\r\n'));
     const invalids = targets.filter(e => !e.alive).map(e => e.name);
     targets = targets.filter(e => e.alive).map(e => e.name);
     const whitelist = await parseWhitelist(process.argv[3]);
     
     const protected = [];
-    const results = [];
+    let results = [];
 
-    for (const name of targets) {
+    if (filterWhitelist) {
+        for (const name of targets) {
 
-        if (whitelist.includes(name)) {
-            protected.push(name);
-        } else {
-            results.push(name);
+            if (whitelist.includes(name)) {
+                protected.push(name);
+            } else {
+                results.push(name);
+            }
         }
+    } else {
+        results = targets;
     }
 
     console.log(`Whitelist: ${protected.join('\n')}\nInvalid: ${invalids.join('\n')}`);
@@ -25,4 +29,4 @@ const parseWhitelist = require('./parseWhitelist');
     fs.writeFile(filename, results.join('\r\n'), err => {
         if (err) console.log(`FileIO error occurred: ${err.message}`);
     });
-})(process.argv[2]);
+})(process.argv[2], process.argv[3]);
